df7fcc3d8e959aa2280749d0c20344f59a1ff2cb
Harshit-018 <54761651+Harshit-018@users.noreply.github.com>
2022-01-21 23:50:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.03%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        
-        for (int i=1;i<address.size();i++)
-        {
-            if (address.at(i)=='.')
-            {
-                address.insert(i+1,"]");
-                address.insert(i,"[");
-                i+=2;
-            }
+        string res;
+        for(auto c: address) {
+            if(c=='.') {
+                res+="[.]";
+            } else res+=c;
         }
-        return address;
+        return res;
     }
 };
