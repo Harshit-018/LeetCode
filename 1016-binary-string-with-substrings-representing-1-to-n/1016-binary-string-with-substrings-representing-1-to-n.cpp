a5b1b27394b273ee43b657f50478079736a49195
Harshit-018 <54761651+Harshit-018@users.noreply.github.com>
2022-02-05 00:16:37 +0530

    Time: 2 ms (59.08%), Space: 6.3 MB (69.16%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -17,7 +17,7 @@ public:
         for (int i=1;i<=n;i++)
         {
             string temp=dectobin(i);
-            if(s.find(temp)== string::npos)
+            if(s.find(temp)== -1)
                 return false;
         }
         return true;
