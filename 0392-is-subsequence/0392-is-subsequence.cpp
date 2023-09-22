18835e4579db359c900304efac723fc32c7adfc4
Harshit Agrahari <agrahariharshit07@gmail.com>
2023-09-23 00:31:00 +0530

    Time: 4 ms (10.63%), Space: 7.1 MB (7.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,14 +4,26 @@ public:
         int n = s.length();
         int m = t.length();
         
-        int i=0, j=0;
+        map<char, vector<int>> mp;
         
-        while(i<n && j<m){
-            if(s[i]==t[j]){
-                i++;
-            }
-            j++;
+        for(int i=0; i<m; i++){
+            mp[t[i]].push_back(i);
         }
-        return i == n;
+        
+        int prev = -1;
+        for(int j=0; j<n; j++){
+            char ch = s[j];
+            
+            if(mp.find(ch)==mp.end()) return false;
+            
+            vector<int> index = mp[ch];
+            
+            auto it = upper_bound(begin(index), end(index), prev);
+            
+            if(it == index.end()) return false;
+            
+            prev = *it;
+        }
+        return true;
     }
 };
