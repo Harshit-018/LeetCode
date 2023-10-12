3c6eaf78fb2a64b292bcc6dd1032768ec99da416
Harshit Agrahari <agrahariharshit07@gmail.com>
2023-10-13 00:00:42 +0530

    Time: 3 ms (78.78%), Space: 7.2 MB (52.99%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -1,75 +1,62 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
 class Solution {
 public:
-    //This function is already taught in my YouTube Video - https://www.youtube.com/watch?v=Op07kT-LoH8 ("Peak Index in a Mountain Array")
     int peakIndexInMountainArray(MountainArray &mountainArr) {
-        int n = mountainArr.length();
-        
+        int r = mountainArr.length()-1;
         int l = 0;
-        int r = n-1;
-        
-        while(l < r) {
-            
+        while(l<r){
             int mid = l + (r-l)/2;
-            
-            if(mountainArr.get(mid) < mountainArr.get(mid+1))
-                l = mid+1;
-            else
-                r = mid;
-            
+                
+            if(mountainArr.get(mid)<mountainArr.get(mid+1))
+               l = mid+1;
+               else r = mid;
         }
-        
-        return l;
+        return l;        
+    }
+               
+    int bsearch(MountainArray &mountainArr, int l, int r, int target){
+        int mid;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(mountainArr.get(mid)==target) return mid;   
+            else if(mountainArr.get(mid)< target)
+                l = mid+1;
+            else r = mid-1;
+        }
+        return -1;  
     }
     
-    int binarySearch(MountainArray &mountainArr, int l, int r, int target) {
-        
+    int revbsearch(MountainArray &mountainArr, int l, int r, int target){
         int mid;
-        while(l <= r) {
-            mid = l + (r-l)/2;
-            
-            if(mountainArr.get(mid) == target) {
-                return mid;
-            } else if(mountainArr.get(mid) > target) {
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(mountainArr.get(mid)==target) return mid;   
+            else if(mountainArr.get(mid)< target)
                 r = mid-1;
-            } else {
-                l = mid+1;
-            }
+            else l = mid+1;
         }
-        
-        return -1;
-        
-    }
-
-    int reverseBinarySearch(MountainArray &mountainArr, int l, int r, int target) {
-        int mid;
-        while(l <= r) {
-            mid = l + (r-l)/2;
-            
-            if(mountainArr.get(mid) == target) {
-                return mid;
-            } else if(mountainArr.get(mid) > target) { //Because the array in right side of peak is in descending order
-                l = mid+1;
-            } else {
-                r = mid-1;
-            }
-        }
-        
-        return -1;
-        
-    }
-    
+        return -1;  
+    }            
+               
     int findInMountainArray(int target, MountainArray &mountainArr) {
         int n = mountainArr.length();
         
-        int peakIndex = peakIndexInMountainArray(mountainArr);
+        int ind = peakIndexInMountainArray(mountainArr);
         
-        int idx = binarySearch(mountainArr, 0, peakIndex, target);
+        int ans_ind = bsearch(mountainArr,0,ind,target);
         
-        if(idx == -1) {
-            return reverseBinarySearch(mountainArr, peakIndex, n-1, target);
-        }
+        if(ans_ind != -1) return ans_ind;
         
-        return idx;
-        
+        return ans_ind = revbsearch(mountainArr,ind,n-1,target);
     }
+              
 };
